caa88454c594538125d2600edd36108acac02aba
Samuelk-Chase <samuelkchase@icloud.com>
2025-05-30 10:04:25 -0600

    change of name from email

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -82,7 +82,7 @@ const Auth = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="mt-1 block w-full px-3 sm:px-4 py-2 sm:py-3 rounded-lg border border-gray-300 shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-150 ease-in-out"
-                placeholder="Enter your email"
+                placeholder="Enter your name"
                 required
               />
             </div>
